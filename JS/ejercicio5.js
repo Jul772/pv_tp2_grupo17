ffdbe8e00df2cb8541a9ba6f48ba61e9d0f422ed
Franco 2 <xlxfrankxlx+1@gmail.com>
2025-04-14 20:47:35 -0300

    agregando punto 2 y 5

---
+++ JS/ejercicio5.js
@@ -0,0 +1,5 @@
+const horas = 10;
+const minutos = 10;
+let segundos
+segundos = (horas * 3600) + (minutos * 60);
+console.log(`La cantidad de segundos en ${horas} horas y ${minutos} minutos es: ${segundos}`);
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # pv_tp2_grupo17
 
 1. Julian Enrique Armella - usuario: Jul772
+2. Franco German Cruz - usuario: 013Frank
