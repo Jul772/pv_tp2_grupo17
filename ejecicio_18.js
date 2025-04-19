9b63e3e947c1f2dd95a2f34537000759a6204149
Jul772 <Juenares13@gmail.com>
2025-04-19 16:22:11 -0300

    puntos 12,18,22 complertos

---
+++ ejecicio_18.js
@@ -0,0 +1,12 @@
+// Función para cambiar el texto
+function cambiarTexto(nuevoTexto) {
+    const parrafo = document.getElementById("miParrafo");
+    parrafo.textContent = nuevoTexto;
+  }
+  
+  // botón
+  const boton = document.getElementById("miBoton");
+  boton.addEventListener("click", () => {
+    cambiarTexto("¡Texto cambiado correctamente!");
+  });
+  
